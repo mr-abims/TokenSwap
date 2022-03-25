05f63313ba89c2c05bb5214bcb04a3a4210de6e3
mr-abims <adebayoabimbola9@gmail.com>
2022-03-25 06:18:40 +0100

    update

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -2,37 +2,52 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
+import "./IERC20.sol";
 contract TokenSwap {
     AggregatorV3Interface internal priceFeed;
+    uint8 decimals;
+    int256 currentRate;
+    address internal daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address internal usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    struct Swap {
+        uint256 amountIn;
+        address owner;
+        uint8 currencyDecimal;
+        bool status;
+    }
+    mapping (uint => Swap) swapOrder;
+    IERC20 DAI = IERC20(daiAddress);
+    IERC20 USDC = IERC20(usdcAddress);
 
-    /**
-     * Network: Kovan
-     * Pair Name: DAI / ETH	
-     18	
-     0x22B58f1EbEDfCA50feF632bD73368b2FdA96D5414e99
+   
+    /**  
+    0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9 * Network: Mainet * Pair Name: DAI / USD * 
+     
+     
      */
     constructor() {
         priceFeed = AggregatorV3Interface(
-            0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541
+            0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9
         );
     }
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int256) {
+    function getLatestPrice() public {
         (
-            ,
-            /*uint80 roundID*/
-            int256 price,
-            ,
-            ,
-
-        ) = /*uint startedAt*/
-            /*uint timeStamp*/
-            /*uint80 answeredInRound*/
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+            ) =
             priceFeed.latestRoundData();
-        return price;
-    }
+            currentRate = price;
+    }    
+    function getRate() public view returns(int256, uint8) {
+        return(currentRate, decimals);
+    }     
+
+
 }
